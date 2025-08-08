0d5eb21a81515ca5eaed203e2a86cc2805475ec5
John Doe <johndoe@example.com>
2025-08-07 20:06:12 -0700

    update Thu 08/07/2025

--- src/a_integers.c
+++ src/a_integers.c
@@ -19,3 +19,23 @@ void lc_integers_parse_digits_to_array(int source, unsigned char* dest_array, si
         source = source / 10;
     }
 }
+
+int lc_integers_join_digits_from_array(const unsigned char* src_array, size_t src_array_size) {
+    int result = 0;
+    for (size_t i = 0; i < src_array_size; i++) {
+        result = result * 10 + src_array[src_array_size - i - 1];
+    }
+    return result;
+}
+
+
+void lc_print_integer_array(const unsigned char* src_array, size_t src_array_size) {
+    printf("[");
+    for (size_t i = 0; i < src_array_size; i++) {
+        printf("%u", src_array[i]);
+        if (i + 1 < src_array_size) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+}
